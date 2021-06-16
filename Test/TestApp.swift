9268e077bb1adccb9ca64cfc8ae711ff285ac283
stefanji <408776303@qq.com>
2021-06-16 20:22:55 +0800

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by JY on 2021/6/16.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
